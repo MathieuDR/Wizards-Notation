9e12346a73a77f014ff3c0bfe1e1663cd2e11c01
MathieuDR <mathieu@deraedt.dev>
2025-05-24 13:13:09 +0200

    spell attributes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
   import Option from "$lib/components/Option.svelte";
   const levels = [
+    // k1
     "Cantrip",
     "Level 1",
     "Level 2",
@@ -14,6 +15,7 @@
   ];
 
   const schools = [
+    // k2
     "Abjuration",
     "Conjuration",
     "Divination",
@@ -24,21 +26,22 @@
     "Transmutation",
   ];
 
-  const time = [
-    "Action",
-    "Bonus Action",
-    "Reaction",
-    "1 Minute",
-    "10 Minute",
-    "1 Hour",
-    "8 Hours",
-    "12 Hours",
-    "24 Hours",
-  ];
+  // const time = [
+  //   "Action",
+  //   "Bonus Action",
+  //   "Reaction",
+  //   "1 Minute",
+  //   "10 Minute",
+  //   "1 Hour",
+  //   "8 Hours",
+  //   "12 Hours",
+  //   "24 Hours",
+  // ];
 
   const duration = [
+    //k6
     "Instantaneous",
-    "Round",
+    "1 Round",
     "1 Minute",
     "10 Minute",
     "1 Hour",
@@ -46,9 +49,12 @@
     "12 Hours",
     "24 Hours",
     "Longer",
+    "Until Dispelled",
+    "Until Dispelled or Triggered",
   ];
 
   const damage = [
+    //k3
     "None",
     "Acid",
     "Bludgeoning",
@@ -64,21 +70,36 @@
     "Slashing",
     "Thunder",
   ];
+
+  // Area of effect
+  const aoe = [
+    //k4
+    "Cone",
+    "Cylinder",
+    "Line",
+    "Cube",
+    "Emanation",
+    "Sphere",
+  ];
+
+  // k5
+  const range = ["Self", "Touch", "Ranged", "Sight", "Unlimited"];
 </script>
 
 <div class="flex flex-col gap-8 flex-1">
   <div id="ctx" class="gap-8 flex flex-1">
     <div id="controls" class="flex-none w-1/3">
       <fieldset
-        class="fieldset bg-base-200 border-base-300 p-4 rounded-box border"
+        class="fieldset bg-base-200 border-base-300 p-4 rounded-box border max-w-64"
       >
         <legend class="fieldset-legend">Spell info</legend>
 
-        <Option name="Level" values={levels}></Option>
+        <Option name="Level" startsAtZero values={levels}></Option>
         <Option name="School" values={schools}></Option>
-        <Option name="Time" values={time}></Option>
-        <Option name="Duration" values={duration}></Option>
         <Option name="Damage Type" startsAtZero values={damage}></Option>
+        <Option name="Area of Effect" values={aoe}></Option>
+        <Option name="Range" values={range}></Option>
+        <Option name="Duration" values={duration}></Option>
       </fieldset>
     </div>
     <div id="canvas" class="flex-1 w-2/3">CANVAS</div>
