72ce9286eab670f63e8080247f78544dab0283fa
MathieuDR <mathieu@deraedt.dev>
2025-05-27 01:22:24 +0200

    cleanup

--- src/lib/components/Notation.svelte
+++ src/lib/components/Notation.svelte
@@ -50,15 +50,14 @@
 
   function drawGraph(graph: Map<number, number[]>, path: pathOptions) {
     graph.forEach((value: number[], key: number) => {
-      console.log(`Key: ${key}, values: ${value.join(", ")}`);
       let a = options.drawing.flipped ? path.dots - key : key;
       const { x: ax, y: ay } = circleCoords(a, path);
       value.forEach((dest) => {
         let b = options.drawing.flipped ? path.dots - dest : dest;
         const { x: bx, y: by } = circleCoords(b, path);
-        const linePoints = [ax + 10, ay + 10, bx + 10, by + 10];
 
-        const line = new Line(linePoints, {
+        // TODO: These +10 are the radius of the circle, hardcoded sucks
+        const line = new Line([ax + 10, ay + 10, bx + 10, by + 10], {
           fill: "#1e1e2e",
           selectable: false,
           evented: false,
